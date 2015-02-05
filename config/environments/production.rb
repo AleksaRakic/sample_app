df22983f70f0b2488d02f68fa7953a3820c44544
Aleksa <rakicaleksa@ymail.com>
2015-02-05 16:55:58 +0100

    sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,8 +82,8 @@ Rails.application.configure do
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
+    :user_name      => ENV['app32767939@heroku.com'],
+    :password       => ENV['bogaqnry'],
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
