ae9ecb64455698162bd2edfd13bc4d1ecf05bc85
Aleksa <rakicaleksa@ymail.com>
2015-02-11 00:55:37 +0100

    poslednji pokusaj

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,8 +3,8 @@ if Rails.env.production?
     config.fog_credentials = {
       # Configuration for Amazon S3
       :provider              => 'AWS',
-      :aws_access_key_id     => ENV['AKIAIUIQCPEZXUW4XXNA'],
-      :aws_secret_access_key => ENV['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']
+      :aws_access_key_id     => ENV['AKIAIRXEZ7WCVB5I5C6Q'],
+      :aws_secret_access_key => ENV['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']
     }
     config.fog_directory     =  ENV['plazma']
   end
