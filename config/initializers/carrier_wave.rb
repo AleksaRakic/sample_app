50e1036cc757994ba7767955f0e283fc0035c0c5
Aleksa <rakicaleksa@ymail.com>
2015-02-11 21:34:31 +0100

    carrierWave

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,9 +3,9 @@ if Rails.env.production?
     config.fog_credentials = {
       # Configuration for Amazon S3
       :provider              => 'AWS',
-      :aws_access_key_id     => ENV['AKIAIRXEZ7WCVB5I5C6Q'],
-      :aws_secret_access_key => ENV['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']
+      :aws_access_key_id     => ENV['AKIAIDYRTYHHTK44LN7A'],
+      :aws_secret_access_key => ENV['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']
     }
-    config.fog_directory     =  ENV['plazma']
+    config.fog_directory     =  ENV['vaterpolo']
   end
 end
